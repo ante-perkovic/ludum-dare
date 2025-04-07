354fa1ae272e1d90f0e9f2757b16a05d213fd069
Luka <ivkalu@gmail.com>
2025-04-08 01:31:44 +0200

    change font for enemy and coin labels

--- ludumdaregame/Collectibles/Coin/label.gd
+++ ludumdaregame/Collectibles/Coin/label.gd
@@ -1,4 +1,11 @@
 extends Label
 
 func _ready():
-	pass
+	
+	var timer = Timer.new()
+	timer.wait_time = 1
+	timer.one_shot = true
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(queue_free)
+	
