e74fcf0149defbbb37cc7072d4a1d2590ac8c380
Antonio <antonio.lakos1@gmail.com>
2025-04-07 17:22:06 +0200

    label added after you pick coints up

--- ludumdaregame/Collectibles/Coin/coin.gd
+++ ludumdaregame/Collectibles/Coin/coin.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
-
+var FloatingTextScene = preload("res://Collectibles/Coin/Label.tscn")  # Adjust the path as needed
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.add_coin()
+		# Instance the floating text and set its position
+		var floatingText = FloatingTextScene.instantiate()
+		# Position it where the coin was, or adjust if needed for your UI
+		floatingText.global_position = position  
+		# Add to the scene tree. You might want to add it to a CanvasLayer if you need it to render above everything.
+		get_tree().get_root().add_child(floatingText)
+
 		queue_free()
